f81139b859ecd198e05c7e14e07f5859b201a323
Chase Roohms <chaseroohms@gmail.com>
2026-02-20 20:48:34 -0600

    docs: update redoc endpoint to use root path and comment out swagger endpoint

--- backend/api/routes/docs.py
+++ backend/api/routes/docs.py
@@ -5,7 +5,7 @@ from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
 router = APIRouter(prefix="/docs", tags=["docs"], redirect_slashes=True)
 settings = get_settings()
 
-@router.get("/redocs/", include_in_schema=False)
+@router.get("/", include_in_schema=False)
 def overridden_redoc():
     """
     Overrides the default /redoc endpoint to use a custom favicon.
@@ -16,13 +16,13 @@ def overridden_redoc():
         redoc_favicon_url="/icons/beaker-red-bg.png" # Replace with your custom favicon URL
     )
 
-@router.get("/swagger/", include_in_schema=False)
-def overridden_swagger():
-    """
-    Overrides the default /swagger endpoint to use a custom favicon.
-    """
-    return get_swagger_ui_html(
-        openapi_url="/openapi.json",
-        title=f"{settings.app_name} API - Swagger",
-        swagger_favicon_url="/icons/beaker-red-bg.png" # Replace with your custom favicon URL
-    )
+#@router.get("/swagger/", include_in_schema=False)
+#def overridden_swagger():
+#    """
+#    Overrides the default /swagger endpoint to use a custom favicon.
+#    """
+#    return get_swagger_ui_html(
+#        openapi_url="/openapi.json",
+#        title=f"{settings.app_name} API - Swagger",
+#        swagger_favicon_url="/icons/beaker-red-bg.png" # Replace with your custom favicon URL
+#    )
