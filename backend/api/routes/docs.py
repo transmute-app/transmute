7c237193de4484b5a0f6be128bdbbf0b732f12b4
Chase Roohms <chaseroohms@gmail.com>
2026-02-27 15:41:12 -0600

    fix: remove commented-out swagger endpoint logic from docs.py

--- backend/api/routes/docs.py
+++ backend/api/routes/docs.py
@@ -15,14 +15,3 @@ def overridden_redoc():
         title=f"{settings.app_name} API - ReDoc",
         redoc_favicon_url="/icons/beaker-red-bg.png" # Replace with your custom favicon URL
     )
-
-#@router.get("/swagger/", include_in_schema=False)
-#def overridden_swagger():
-#    """
-#    Overrides the default /swagger endpoint to use a custom favicon.
-#    """
-#    return get_swagger_ui_html(
-#        openapi_url="/openapi.json",
-#        title=f"{settings.app_name} API - Swagger",
-#        swagger_favicon_url="/icons/beaker-red-bg.png" # Replace with your custom favicon URL
-#    )
